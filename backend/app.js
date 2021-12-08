5a945859c462cb020799cd56f88ab450e2d2a95b
Abhinav Negi <anegi0742@gmail.com>
2021-12-08 14:06:59 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,16 @@ const path = require('path')
 
 const errorMiddleware = require('./middlewares/errors')
 
+const cors=require("cors");
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions))
+
 // Setting up config file 
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 // dotenv.config({ path: 'backend/config/config.env' })
@@ -16,7 +26,8 @@ if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'bac
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(fileUpload());
+app.use(fileUpload({useTempFiles: true}));
+
 
 
 // Import all routes
